ea39b96033bd965e58e626b5ea7b418fb65a1ab1
bargaouiahmed <bargaouiahmed@gmail.com>
2026-04-10 22:06:14 +0100

    the bug was a nullable fk issue, hopefully it's resolved

--- Backend/Auth/Entities/Student.cs
+++ Backend/Auth/Entities/Student.cs
@@ -8,7 +8,7 @@ public class Student
     public Guid IdentityId { get; set; }
     public AuthIdentity? Identity { get; set; } 
 
-    public Guid UniClassId { get; set; }
+    public Guid? UniClassId { get; set; }
     public UniClass? UniClass { get; set; }    
     public string Firstname { get; set; } = string.Empty;
     public string Lastname { get; set; } = string.Empty;
